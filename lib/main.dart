548eff73fcf7efe7b5544ed139ff3b993e263490
Chairun Nisaq <145679533+broomrun@users.noreply.github.com>
2025-10-22 00:34:03 +0700

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
+import 'package:katahari/pages/splashscreen.dart';
 import 'package:katahari/wrapper.dart';
 
 void main() async {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Wrapper(),
+      home: SplashScreen(),
     );
   }
 }
