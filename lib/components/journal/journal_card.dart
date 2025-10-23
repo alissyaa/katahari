d47fc69466f4c124049f9e6acb9f3d0a704f2c38
alissya <143946274+alissyaa@users.noreply.github.com>
2025-10-23 15:39:29 +0700

    nyicil jurnal

---
+++ lib/components/journal/journal_card.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class JournalCard extends StatelessWidget {
+  final Map<String, dynamic> entry;
+
+  const JournalCard({super.key, required this.entry});
+
+  IconData getMoodIcon(String? mood) {
+    switch (mood) {
+      case 'happy':
+        return Icons.sentiment_very_satisfied;
+      case 'angry':
+        return Icons.sentiment_very_dissatisfied;
+      case 'neutral':
+        return Icons.sentiment_neutral;
+      default:
+        return Icons.sentiment_satisfied;
+    }
+  }
+
+  Color getMoodColor(String? mood) {
+    switch (mood) {
+      case 'happy':
+        return Colors.green;
+      case 'angry':
+        return Colors.red;
+      case 'neutral':
+        return Colors.black;
+      default:
+        return Colors.blue;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: entry['image'] == null ? const Color(0xFFF0F0F0) : Colors.transparent,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.black, width: 2),
+        image: entry['image'] != null
+            ? DecorationImage(
+                image: NetworkImage(entry['image']),
+                fit: BoxFit.cover,
+              )
+            : null,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(18),
+        child: Stack(
+          children: [
+            if (entry['image'] != null)
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.black.withOpacity(0.7), Colors.transparent],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: entry['image'] == null
+                    ? MainAxisAlignment.spaceBetween
+                    : MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (entry['image'] == null)
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Icon(
+                        getMoodIcon(entry['mood']),
+                        color: getMoodColor(entry['mood']),
+                        size: 30,
+                      ),
+                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        entry['title'],
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: entry['image'] != null
+                              ? Colors.white
+                              : Colors.black,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        entry['date'],
+                        style: GoogleFonts.poppins(
+                          fontSize: 12,
+                          color: entry['image'] != null
+                              ? Colors.white70
+                              : Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            if (entry['image'] != null)
+              Positioned(
+                top: 10,
+                right: 10,
+                child: Container(
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.3),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    getMoodIcon(entry['mood']),
+                    color: getMoodColor(entry['mood']),
+                    size: 24,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/journal_page.dart
+++ lib/pages/journal_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class journalpage extends StatefulWidget {
   const journalpage({super.key});
@@ -16,17 +17,65 @@ class _journalpageState extends State<journalpage> {
     await FirebaseAuth.instance.signOut();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Homepage"),),
-      body: Center(
-        child: Text('${user!.email}'),
+      appBar: AppBar(
+        title: Text("Hello" '${user!.email}'),
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: (()=>signout()),
-          child: Icon(Icons.login_rounded),
+      floatingActionButton: FloatingActionButton.large(
+        onPressed: () {},
+        child: FaIcon(FontAwesomeIcons.plus),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Search...',
+                prefixIcon: Icon(Icons.search),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+            ),
+            Expanded(
+              child: GridView.builder(
+                itemCount: 15,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 4,
+                    mainAxisSpacing: 4,
+                  ),
+                  itemBuilder: (context, int index){
+                    return Container(
+                      child: Column(
+                        children: [
+                          Text('judulnya'),
+                          Row(
+                            children: [
+                              Container(
+                                child: Text('first'),
+                              ),
+                            ],
+                          ),
+                          Text('some content'),
+                          Row(
+                            children: [
+                              Text('02 Nov 2025'),
+                              FaIcon(FontAwesomeIcons.trash)
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
