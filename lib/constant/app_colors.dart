bd959cff945fefaa5ec7758ce8921c24516d40e0
Chairun Nisaq <145679533+broomrun@users.noreply.github.com>
2025-11-14 14:43:32 +0700

    AppColor

---
+++ lib/constant/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFF3F3F3);
+  static const Color secondary = Color(0xFF0C1212);
+  static const Color button = Color(0xFFA9CCEF);
+  static const Color screen1 = Color(0xFFD4ECC6);
+  static const Color screen2 = Color(0xFFFFE7AD);
+  static const Color textfield = Color(0xFFAFAFAF);
+  static const Color merah = Color(0xFFFFA1A1);
+  static const Color abu = Color(0xFF323232);
+  static const Color abumuda = Color(0xFF858585);
+}
